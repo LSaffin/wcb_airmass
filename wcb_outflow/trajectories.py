832d044dae0dfea93b60ef5e114b3bcd7dd741e7
Leo Saffin <l.saffin@leeds.ac.uk>
2020-09-09 11:32:52 +0100

    Preprocess winds to a common grid for trajectory calculations

--- wcb_outflow/trajectories.py
+++ wcb_outflow/trajectories.py
@@ -1,8 +1,36 @@
 import numpy as np
 
+import iris
+
+from irise import convert, interpolate
 from pylagranto import caltra
 
 from . import case_studies, data_path
+
+
+def preprocess_winds(case):
+    """Interpolate wind fields to a common grid ready for trajectory calculations
+    """
+    # Don't load pressure on model theta levels
+    pressure_cs = iris.Constraint(
+        cube_func=lambda x: str(x.attributes["STASH"] != "m01s00i407"))
+
+    mapping = case_studies[case].time_to_filename_mapping()
+
+    for time in mapping:
+        cubes = iris.load(mapping[time], iris.Constraint(time=time) & pressure_cs)
+
+        w = convert.calc("upward_air_velocity", cubes)
+        theta = convert.calc("air_potential_temperature", cubes)
+
+        newcubes = iris.cube.CubeList([w, theta])
+        for variable in ["x_wind", "y_wind"]:
+            cube = convert.calc(variable, cubes)
+            cube = interpolate.remap_3d(cube, w)
+            newcubes.append(cube)
+
+        filename = time.strftime("%Y%m%d_%H") + "_winds.nc"
+        iris.save(cubes, str(case_studies[case].data_path / filename))
 
 
 def isentropic_trajectories(case, theta_levels):
