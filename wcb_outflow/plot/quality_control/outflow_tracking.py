c5b60bf4ba7bd1e846f80b2579f5e79a0b3b6698
Leo Saffin <l.saffin@leeds.ac.uk>
2021-01-27 10:55:25 +0000

    Plot outflow region vs time to find find problems with tracking

---
+++ wcb_outflow/plot/quality_control/outflow_tracking.py
@@ -0,0 +1,67 @@
+"""
+Show the outflow region as function of time when tracked backwards with the isentropic
+trajectories. It is useful to see how much the contour breaks down over time
+
+Usage:
+    outflow_tracking.py <case> <theta_level>
+    outflow_tracking.py  (-h | --help)
+
+Arguments:
+    <case>         Name of case study
+    <theta_level>  Which outflow theta level to show (K)
+
+Options:
+    -h --help      Show help
+"""
+import docopt
+
+import matplotlib.pyplot as plt
+
+import iris
+
+from pylagranto import trajectory
+
+from wcb_outflow import case_studies
+from wcb_outflow.plot import background_map, projection
+
+
+def main():
+    arguments = docopt.docopt(__doc__)
+
+    case = case_studies[arguments["<case>"]]
+    theta_level = float(arguments["<theta_level>"])
+
+    fig = plt.figure(figsize=[8, 5])
+
+    ntimes = (case.outflow_time - case.start_time) // case.timestep
+    times = [case.start_time + n*case.timestep for n in range(ntimes+1)]
+
+    tr = trajectory.load(case.data_path / "isentropic_trajectories_backward.pkl")
+    tr_theta = tr.select(
+        "air_potential_temperature", "==", theta_level,
+        time=[tr.relative_times[0]]
+    )
+
+    for n, time in enumerate(times):
+        print(time)
+        pv = iris.load_cube(
+            case.filename_theta(time=time),
+            iris.Constraint(
+                name="ertel_potential_vorticity",
+                time=time,
+                air_potential_temperature=theta_level,
+            )
+        )
+        crs = pv.coord(axis="x").coord_system.as_cartopy_crs()
+        ax = plt.subplot(ntimes // 2, 3, n+1, projection=projection)
+        background_map(ax, pv)
+
+        idx = tr.times.index(time)
+        plt.plot(tr_theta.x[:, idx] - 360, tr_theta.y[:, idx], transform=crs)
+        ax.set_title(time)
+    fig.suptitle("{}: {:.0f}K".format(case.name, theta_level))
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
